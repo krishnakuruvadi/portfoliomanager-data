384430436217f6dbf56cc6739b9cb4858dc6153f
Krishna Kuruvadi <krishnakuruvadi@gmail.com>
2025-03-10 08:39:00 -0700

    more updates to nse_bse_eq.json

--- India/code/update_share.py
+++ India/code/update_share.py
@@ -77,9 +77,50 @@ def merge_new_info(download_dir):
     print(f"Updated JSON has been saved to {orig_file_path}")
 
 
+def copy_selected_fields(download_dir):
+    orig_file_path = os.path.join(str(pathlib.Path(__file__).parent.parent.absolute()), 'nse_bse_eq.json')
+    new_file_path = os.path.join(download_dir, 'nse_bse_eq.json')
+    # Load the first JSON file into a dictionary
+    with open(new_file_path, 'r') as file1:
+        dict1 = json.load(file1)
+
+    # Load the second JSON file into a dictionary
+    with open(orig_file_path, 'r') as file2:
+        dict2 = json.load(file2)
+
+    # Loop through each key in dict1
+    for key, value in dict1.items():
+        if key in dict2:
+            # Check if the 'bse_security_id', 'bse_security_code', and 'nse_symbol' match
+            if (dict1[key].get('bse_security_id') == dict2[key].get('bse_security_id') and
+                dict1[key].get('bse_security_code') == dict2[key].get('bse_security_code') and
+                dict1[key].get('nse_symbol') == dict2[key].get('nse_symbol')):
+                
+                # Copy 'bse_name', 'status', and 'industry' from dict1 to dict2
+                dict2[key]['bse_name'] = value.get('bse_name')
+                dict2[key]['status'] = value.get('status')
+                dict2[key]['industry'] = value.get('industry')
+                if value.get('listing_date','').strip() != '':
+                    dict2[key]['listing_date'] = value.get('listing_date')
+                if value.get('cap','').strip() != '':
+                    dict2[key]['cap'] = value.get('cap')
+                if value.get('face_value','').strip() != '':
+                    dict2[key]['face_value'] = value.get('face_value')
+
+    # Write the updated dictionary back to the second JSON file
+    with open(orig_file_path, 'w') as file2:
+        json.dump(dict2, file2, indent=1)
+
+    print("Selected fields copied successfully.")
+
+
 
 if __name__ == "__main__":
     update_nse(download_dir())
     update_bse(download_dir())
-    merge_new_info(download_dir())
+    # You could merge whole info or select fields
+    # choose this for whole info
+    #merge_new_info(download_dir())
+    # choose this for select fields
+    #copy_selected_fields(download_dir())
     
